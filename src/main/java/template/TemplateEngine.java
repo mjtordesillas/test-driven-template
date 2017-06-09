89607b7e4fb7589d290a963fcd2b327dc8c683df
mjtordesillas <mjtordesillas@gmail.com>
2017-06-09 08:51:47 +0200

    wrote first test (TemplateEngineShould evaluateOneVariable)

--- src/main/java/template/TemplateEngine.java
+++ src/main/java/template/TemplateEngine.java
@@ -2,4 +2,15 @@ package template;
 
 public class TemplateEngine {
 
+    public TemplateEngine(String templateText) {
+
+    }
+
+    public void set(String variable, String value) {
+
+    }
+
+    public String evaluate() {
+        return null;
+    }
 }
--- src/test/java/unit_tests/TemplateEngineShould.java
+++ src/test/java/unit_tests/TemplateEngineShould.java
@@ -1,11 +1,17 @@
 package unit_tests;
 
 import org.junit.Test;
+import template.TemplateEngine;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TemplateEngineShould {
     @Test
-    public void oneVariable() throws Exception {
-
+    public void evaluateOneVariable() throws Exception {
+        TemplateEngine templateEngine = new TemplateEngine("Hello, ${name}");
+        templateEngine.set("name", "Reader");
+        assertThat(templateEngine.evaluate(), is("Hello, Reader"));
     }
 
 }
