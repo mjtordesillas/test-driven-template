9dafcc08fb98dbabd730153717a8a7ed75ee1027
mjtordesillas <mjtordesillas@gmail.com>
2017-06-12 09:03:18 +0200

    refactor: extracted methods for setup and assertions

--- src/test/java/unit_tests/TemplateParserShould.java
+++ src/test/java/unit_tests/TemplateParserShould.java
@@ -3,10 +3,12 @@ package unit_tests;
 import org.junit.Test;
 import template.TemplateParser;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class TemplateParserShould {
 
@@ -14,19 +16,22 @@ public class TemplateParserShould {
     public void returnEmptyWhenReceivingAnEmptyTemplate() throws Exception{
         List<String> segments = parseTemplate("");
 
-        assertThat("Number of segments", segments.size(), is(1));
-        assertThat(segments.get(0), is(""));
+        assertSegments(segments, "");
     }
 
     @Test
     public void parseTemplatesWithOnlyPlainText() throws Exception{
         List<String> segments = parseTemplate("plain text");
 
-        assertThat("Number of segments", segments.size(), is(1));
-        assertThat(segments.get(0), is("plain text"));
+        assertSegments(segments, "plain text");
     }
 
     private List<String> parseTemplate(String template) {
         return new TemplateParser().parse(template);
+    }
+
+    private void assertSegments(List<? extends Object> actual, Object... expected) {
+        assertThat("Number of segments does not match", actual.size(), is(expected.length));
+        assertEquals(Arrays.asList(expected), actual);
     }
 }
