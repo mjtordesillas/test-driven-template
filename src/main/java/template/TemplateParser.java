aeb615ea976582b2cbea5188b946486d9cad91e8
mjtordesillas <mjtordesillas@gmail.com>
2017-06-12 08:53:50 +0200

    template parser parses a template as long as the template contains an empty string or only plain text

---
+++ src/main/java/template/TemplateParser.java
@@ -0,0 +1,10 @@
+package template;
+
+import java.util.Collections;
+import java.util.List;
+
+public class TemplateParser {
+    public List<String> parse(String templateText) {
+        return Collections.singletonList(templateText);
+    }
+}
